def5dcc67c4f39716a39c3bcaf2dddc437a5e091
Felipe Ardila <fardila@dhcp-67-95.ucsc.edu>
2019-07-02 16:24:46 -0700

    ran on reduced halo and particle catalogs. now takes less than 5min compared to 2hours per run.

--- example.py
+++ example.py
@@ -9,9 +9,11 @@ par = bfc.par()
 
 #input-output files
 par.files.transfct          = "/Users/fardila/Documents/GitHub/baryonification/baryonification/files/CDM_PLANCK_tk.dat"
-par.files.halofile_in       = "/Users/fardila/Documents/Data/baryonic_effects/input/halo_catalogs/um_smdpl_insitu_exsitu_0.7124_basic_logmp_11.5.npy"
+# par.files.halofile_in       = "/Users/fardila/Documents/Data/baryonic_effects/input/halo_catalogs/um_smdpl_insitu_exsitu_0.7124_basic_logmp_11.5.npy"
+par.files.halofile_in       = "/Users/fardila/Documents/Data/baryonic_effects/input/halo_catalogs/um_smdpl_insitu_exsitu_0.7124_basic_logmp_11.5_cutMpeak13.npy"
 par.files.halofile_format = "ROCKSTAR-NPY"
-par.files.partfile_in       = "/Users/fardila/Documents/Data/baryonic_effects/input/particle_catalogs/um_smdpl_particles_0.7124_50m.npy"
+# par.files.partfile_in       = "/Users/fardila/Documents/Data/baryonic_effects/input/particle_catalogs/um_smdpl_particles_0.7124_50m.npy"
+par.files.partfile_in       = "/Users/fardila/Documents/Data/baryonic_effects/input/particle_catalogs/um_smdpl_particles_0.7124_10m.npy"
 par.files.partfile_format = "huang_npy"
 par.files.partfile_out      = "/Users/fardila/Documents/Data/baryonic_effects/output/particles.out"
 
@@ -46,4 +48,5 @@ print("Running time: {0} seconds".format(time.time() - time1))
 
 ##############################################################################
 # timing
-# 6500 s on single core on Chia (~2hrs) 
+# all halos (1,557,939 halos) + 50M particles : 6500s (~2hrs) on single core on Chia
+# Mpeak>13 halos (45,594 halos) + 10M particles :  291s (<5min) on single core on Chia
